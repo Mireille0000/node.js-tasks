02e26a52907bdae70d20a487def5cede86fab5b1
Gasanova Tamara <solas93@mail.ru>
2024-10-19 00:28:12 +0300

    refactor: add readable stream to findUsers function

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,16 +1,30 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { findUsers } from "../models/users-model";
+import { findUserById, findUsers } from "../models/users-model";
 import { UserData } from "../utils/interfaces";
+import {createReadStream } from "node:fs";
 
 
 export const getUsers = async(req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
     try {
         const users: UserData[] = await findUsers();
         res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(JSON.stringify({users: users, message: 'Request Completed Successfully Get'}))
-        
+        res.end(JSON.stringify({users: users, message: 'Request Completed Successfully Get'}))  
     } catch (error) {
         console.error(error);
     }
-
 }
+
+// export const getUserById = async(req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+//     try {
+//         const user: UserData = await findUserById();
+//         if(user) {
+//             res.writeHead(200, {'Content-type': 'application/json'});
+//             res.end(JSON.stringify({users: user, message: 'Request Completed Successfully (Get)'}))  
+//         } else {
+//             res.writeHead(404, {'Content-type': 'application/json'});
+//             res.end(JSON.stringify({message: 'User Not Found (GetById)'})) 
+//         } 
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
--- src/models/users-model.ts
+++ src/models/users-model.ts
@@ -1,12 +1,19 @@
 import { UserData } from "../utils/interfaces";
 
+import { createReadStream } from "node:fs";
+
 export const findUsers = (): Promise<UserData[]> => {
     return new Promise((resolve, reject) => {
-        resolve([{
-            id: '',
-            username: '',
-            age: 0,
-            hobbies: ['']
-        }]);
+        let users = createReadStream('src/data/users-data.json');
+        users.on("data", (data) => {
+            resolve(JSON.parse(data as string));
+        })
+
+    })
+}
+
+export const findUserById = (id: string): Promise<UserData> => {
+    return new Promise((resolve, reject) => {
+        resolve
     })
 }
