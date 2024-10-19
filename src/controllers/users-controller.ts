b355604530efd0e7f38e8fa5a8bc6f7f0b64329e
Gasanova Tamara <solas93@mail.ru>
2024-10-19 14:52:59 +0300

    feat: implement post request

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { findUserById, findUsers } from "../models/users-model";
+import { findUserById, findUsers, postNewUser } from "../models/users-model";
 import { UserData } from "../utils/interfaces";
 import {createReadStream } from "node:fs";
 import { isValid } from "../utils/requests-utils";
@@ -8,8 +8,13 @@ import { isValid } from "../utils/requests-utils";
 export const getUsers = async(req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
     try {
         const users: UserData[] = await findUsers();
-        res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(JSON.stringify({users: users, message: 'Request Completed Successfully Get'}))  
+        if(users) {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(JSON.stringify({users: users, message: 'Request Completed Successfully Get'})) 
+        } else {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(JSON.stringify({users: {}, message: 'Users Not Found Get'})) 
+        }
     } catch (error) {
         console.error(error);
     }
@@ -32,5 +37,30 @@ export const getUserById = async(req: IncomingMessage, res: ServerResponse<Incom
         } 
     } catch (error) {
         console.log(error);
+    }
+}
+
+
+export const postUser = async(req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+    try {
+        let body = '';
+        req.on('data', (data) => {
+            body += data.toString();
+        })
+
+        req.on('end', async() => {
+            const {username, age, hobbies} = JSON.parse(body);
+            const user = {
+                username,
+                age,
+                hobbies
+            };
+            const newUser: UserData = await postNewUser(user);
+
+            res.writeHead(201, {'Content-type': 'application/json'});
+            res.end(JSON.stringify({newUser}))  
+        })
+    } catch (error) {
+        console.error(error);
     }
 }
--- src/models/users-model.ts
+++ src/models/users-model.ts
@@ -1,15 +1,16 @@
 import { UserData } from "../utils/interfaces";
 
-import { createReadStream } from "node:fs";
+import { createReadStream, writeFile } from "node:fs";
 
 import { randomUUID } from 'node:crypto';
 
-export const userId = randomUUID();
+export const userRandomId = randomUUID();
 
 export const findUsers = (): Promise<UserData[]> => {
     return new Promise((resolve, reject) => {
-        let users = createReadStream('src/data/users-data.json');
+        const users = createReadStream('src/data/users-data.json');
         users.on("data", (data) => {
+            console.log(JSON.parse(data as string))
             resolve(JSON.parse(data as string));
         })
 
@@ -18,11 +19,28 @@ export const findUsers = (): Promise<UserData[]> => {
 
 export const findUserById = (id: string): Promise<UserData> => {
     return new Promise((resolve, reject) => {
-        let usersStream = createReadStream('src/data/users-data.json');
+        const usersStream = createReadStream('src/data/users-data.json');
         usersStream.on("data", (data) => {
             const users: UserData[] = JSON.parse(data as string);
+            console.log(users);
             const user = users.find((user) => user.id === id) as UserData;
             resolve(user);
         })
     })
+}
+
+export const postNewUser = (user: UserData): Promise<UserData> => {
+    return new Promise((resolve, reject) => {
+        const {username, age, hobbies} = user
+
+        const newUser = {id: userRandomId, username, age, hobbies};
+        const usersStream = createReadStream('src/data/users-data.json');
+        usersStream.on("data", (data) => {
+           const usersArr = JSON.parse(data as string);
+           usersArr.push(newUser)
+           writeFile('src/data/users-data.json', JSON.stringify(usersArr), () => {});
+           resolve(newUser);
+        })
+    })
+
 }
